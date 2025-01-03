019190bebb323378469f61a696f1336b8b106d33
bengin34 <becaglar3434@gmail.com>
2025-01-03 11:46:03 +0100

    commented lines are deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import DisplayMenu from './components/DisplayMenu';
 import FilterMenu from './components/FilterMenu';
-// import AddToCart from './components/AddToCart';
 import SearchBar from './components/SearchBar';
 import SortMenu from './components/SortMenu';
 
@@ -11,7 +10,6 @@ const App = () => {
             <h1>Reactify Cafe</h1>
             <DisplayMenu />
             <FilterMenu />
-            {/* <AddToCart /> */}
             <SearchBar />
             <SortMenu />
         </div>
